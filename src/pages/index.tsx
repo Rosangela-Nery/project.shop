1ea1e65e73894e1af6b8ac49f6a0d7defc8296d7
Rosângela Nery <rosangela2nery@gmail.com>
2023-02-28 16:38:17 -0300

    Configuring Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+    backgroundColor: '$roxo',
+})
+
 export default function Home() {
     return (
-        <h1>Hello World</h1>
+        <Button>Hello World</Button>
     )
 }
