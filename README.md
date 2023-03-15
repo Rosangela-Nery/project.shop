356ba825558e095378ecbb728b07e79cfa6b7a2d
Rosângela Nery <rosangela2nery@gmail.com>
2023-03-14 21:24:37 -0300

    styling page

---
+++ README.md
@@ -0,0 +1,1 @@
+### Trabalhando com next e stitches;
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -4,12 +4,13 @@ export const Container = styled('div', {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'flex-start',
+    justifyContent: 'center',
     minHeight: '100vh',
 })
 
 export const Header = styled('header', {
     padding: '2rem 0',
     width: '100%',
-    maxWidth: '100vh',
+    maxWidth: '100vw',
     margin: '0 auto',
 })
