33c84c56ea4bd28fe6c8b451b7b6f06e43ea715f
Rosângela Nery <rosangela2nery@gmail.com>
2023-03-23 14:09:13 -0300

    Inserting next.js link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import { GetStaticProps } from "next";
 import { useKeenSlider } from 'keen-slider/react';
 import { HomeContainer, Product } from "../styles/pages/home";
 import Stripe from "stripe";
+import Link from "next/link";
 
 interface HomeProps {
     products: {
@@ -27,13 +28,15 @@ export default function Home({ products }: HomeProps) {
         <HomeContainer ref={sliderRef} className="keen-slider">
             {products.map(product => {
                 return (
-                    <Product key={product.id} className="keen-slider__slide">
-                        <Image src={product.imageUrl} width={520} height={480} unoptimized alt="" />
-                        <footer>
-                            <strong>{product.name}</strong>
-                            <span>{product.price}</span>
-                        </footer>
-                    </Product>
+                    <Link href={`/product/${product.id}`} key={product.id}>
+                        <Product className="keen-slider__slide">
+                            <Image src={product.imageUrl} width={520} height={480} unoptimized alt="" />
+                            <footer>
+                                <strong>{product.name}</strong>
+                                <span>{product.price}</span>
+                            </footer>
+                        </Product>
+                    </Link>
                 )
             })}
         </HomeContainer>
@@ -52,7 +55,10 @@ export const getStaticProps: GetStaticProps = async () => {
             id: product.id,
             name: product.name,
             imageUrl: product.images[0],
-            price: price.unit_amount as number / 100,
+            price: new Intl.NumberFormat('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+            }).format(price.unit_amount as number / 100),
         }
     })
 
