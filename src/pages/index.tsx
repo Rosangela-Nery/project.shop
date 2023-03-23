9a92d7a8351822104c7f78c23241cd81f99c023e
Rosângela Nery <rosangela2nery@gmail.com>
2023-03-23 11:19:11 -0300

    Searching Stripe products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
-
+import 'keen-slider/keen-slider.min.css';
+import { stripe } from "../lib/stripe";
+import { GetServerSideProps } from "next";
 import { useKeenSlider } from 'keen-slider/react';
-
 import { HomeContainer, Product } from "../styles/pages/home";
 
 import produto1 from '../assets/produtos/1.png';
@@ -14,10 +15,18 @@ import produto7 from '../assets/produtos/7.png';
 import produto8 from '../assets/produtos/8.png';
 import produto9 from '../assets/produtos/9.png';
 import produto10 from '../assets/produtos/10.png';
+import Stripe from "stripe";
 
-import 'keen-slider/keen-slider.min.css';
+interface HomeProps {
+    products: {
+        id: string;
+        name: string;
+        imageUrl: string;
+        price: number;
+    }[]
+}
 
-export default function Home(props) {
+export default function Home({ products }: HomeProps) {
     const [sliderRef] = useKeenSlider({
         slides: {
             perView: 3,
@@ -27,97 +36,40 @@ export default function Home(props) {
 
     return (
         <HomeContainer ref={sliderRef} className="keen-slider">
-            <pre>{JSON.stringify(props.list)}</pre>
-
-            <Product className="keen-slider__slide">
-                <Image src={produto1} width={520} height={480} unoptimized alt="" />
-                <footer>
-                    <strong>Saia Curta Alfaiataria</strong>
-                    <span>R$ 79,90</span>
-                </footer>
-            </Product>
-
-            <Product className="keen-slider__slide">
-                <Image src={produto2} width={520} height={480} unoptimized alt="" />
-                <footer>
-                    <strong>Vestido Curto em Veludo</strong>
-                    <span>R$ 99,90</span>
-                </footer>
-            </Product>
-
-            <Product className="keen-slider__slide">
-                <Image src={produto3} width={520} height={480} unoptimized alt="" />
-                <footer>
-                    <strong>Vestido Curto em Veludo</strong>
-                    <span>R$ 99,90</span>
-                </footer>
-            </Product>
-
-            <Product className="keen-slider__slide">
-                <Image src={produto4} width={520} height={480} unoptimized alt="" />
-                <footer>
-                    <strong>Vestido Curto em Veludo</strong>
-                    <span>R$ 99,90</span>
-                </footer>
-            </Product>
-
-            <Product className="keen-slider__slide">
-                <Image src={produto5} width={520} height={480} unoptimized alt="" />
-                <footer>
-                    <strong>Vestido Curto em Veludo</strong>
-                    <span>R$ 99,90</span>
-                </footer>
-            </Product>
-
-            <Product className="keen-slider__slide">
-                <Image src={produto6} width={520} height={480} unoptimized alt="" />
-                <footer>
-                    <strong>Vestido Curto em Veludo</strong>
-                    <span>R$ 99,90</span>
-                </footer>
-            </Product>
-
-            <Product className="keen-slider__slide">
-                <Image src={produto7} width={520} height={480} unoptimized alt="" />
-                <footer>
-                    <strong>Vestido Curto em Veludo</strong>
-                    <span>R$ 99,90</span>
-                </footer>
-            </Product>
-
-            <Product className="keen-slider__slide">
-                <Image src={produto8} width={520} height={480} unoptimized alt="" />
-                <footer>
-                    <strong>Vestido Curto em Veludo</strong>
-                    <span>R$ 99,90</span>
-                </footer>
-            </Product>
-
-            <Product className="keen-slider__slide">
-                <Image src={produto9} width={520} height={480} unoptimized alt="" />
-                <footer>
-                    <strong>Vestido Curto em Veludo</strong>
-                    <span>R$ 99,90</span>
-                </footer>
-            </Product>
-
-            <Product className="keen-slider__slide">
-                <Image src={produto10} width={520} height={480} unoptimized alt="" />
-                <footer>
-                    <strong>Vestido Curto em Veludo</strong>
-                    <span>R$ 99,90</span>
-                </footer>
-            </Product>
+            {products.map(product => {
+                return (
+                    <Product key={product.id} className="keen-slider__slide">
+                        <Image src={product.imageUrl} width={520} height={480} unoptimized alt="" />
+                        <footer>
+                            <strong>{product.name}</strong>
+                            <span>{product.price}</span>
+                        </footer>
+                    </Product>
+                )
+            })}
         </HomeContainer>
     )
 }
 
-export const getServerSideProps = async () => {
-    await new Promise(resolve => setTimeout(resolve, 2000))
+export const getServerSideProps: GetServerSideProps = async () => {
+    const response = await stripe.products.list({
+        expand: ['data.default_price']
+    })
+
+    const products = response.data.map(product => {
+        const price = product.default_price as Stripe.Price
+
+        return {
+            id: product.id,
+            name: product.name,
+            imageUrl: product.images[0],
+            price: price.unit_amount as number / 100,
+        }
+    })
 
     return {
         props: {
-            list: [1, 2, 3]
+            products,
         }
     }
 }
