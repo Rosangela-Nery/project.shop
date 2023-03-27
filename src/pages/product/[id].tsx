a114bb27af0270edffd69ab1165f113761bb6998
Rosângela Nery <rosangela2nery@gmail.com>
2023-03-27 12:12:41 -0300

    Product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
     const { query } = useRouter()
 
     return (
-        <h1>Product</h1>
+        <ProductContainer>
+            <ImageContainer>
+
+            </ImageContainer>
+
+            <ProductDetails>
+                <h1>Camisa x</h1>
+                <span>R$ 79,90</span>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero aliquam id asperiores, tenetur maiores aliquid aspernatur laudantium tempora voluptates non quod. Qui facilis officiis aperiam, assumenda optio blanditiis nostrum tempore?</p>
+
+                <button>
+                    Comprar agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
